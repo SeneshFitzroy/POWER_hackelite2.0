532c08694805eae31e3814ea22b1cce51f554f3b
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-09-06 02:24:16 +0530

    feat: Enhanced src/pos/components/PharmacyPOSFinal.js, src/App.js,  - Detailed changes: diff --git a/src/App.js b/src/App.js index cd8ee56... at  2:24:16.45 Sat 09/06/2025

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
-import PharmacyPOSProfessional from './pos/components/PharmacyPOSProfessional';
+import PharmacyPOSFinal from './pos/components/PharmacyPOSFinal';
 import './App.css';
 
 function App() {
   return (
     <Box sx={{ height: '100vh', overflow: 'hidden' }}>
       <Routes>
-        <Route path="/" element={<PharmacyPOSProfessional />} />
-        <Route path="/pos" element={<PharmacyPOSProfessional />} />
+        <Route path="/" element={<PharmacyPOSFinal />} />
+        <Route path="/pos" element={<PharmacyPOSFinal />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Box>
