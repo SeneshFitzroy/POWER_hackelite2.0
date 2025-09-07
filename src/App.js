7f9d76199a683a832fb18d5de485232e94367fed
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-09-07 20:13:30 +0530

    feat: Enhanced src/App.js,  - Detailed changes: diff --git a/src/App.js b/src/App.js index 1760c17... at 20:13:30.35 Sun 09/07/2025

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { Box } from '@mui/material'
 import SplashScreen from './components/SplashScreen'
 import LoginScreen from './components/LoginScreen'
 import ERPDashboard from './components/ERPDashboard'
+import PharmacyPOSFirebaseIntegrated from './pos/components/PharmacyPOSFirebaseIntegrated'
 import './App.css'
 
 function App() {
@@ -15,12 +18,26 @@ function App() {
     setCurrentScreen('dashboard')
   }
 
+  const handlePOSAccess = () => {
+    window.location.href = '/pos'
+  }
+
   return (
-    <div className="App">
-      {currentScreen === 'splash' && <SplashScreen onGetStarted={handleSplashComplete} />}
-      {currentScreen === 'login' && <LoginScreen onLoginSuccess={handleLoginSuccess} />}
-      {currentScreen === 'dashboard' && <ERPDashboard />}
-    </div>
+    <Routes>
+      <Route path="/pos" element={
+        <Box sx={{ height: '100vh', overflow: 'hidden' }}>
+          <PharmacyPOSFirebaseIntegrated />
+        </Box>
+      } />
+      <Route path="/" element={
+        <div className="App">
+          {currentScreen === 'splash' && <SplashScreen onGetStarted={handleSplashComplete} />}
+          {currentScreen === 'login' && <LoginScreen onLoginSuccess={handleLoginSuccess} />}
+          {currentScreen === 'dashboard' && <ERPDashboard onPOSAccess={handlePOSAccess} />}
+        </div>
+      } />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
 
