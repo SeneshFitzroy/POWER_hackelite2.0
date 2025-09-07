815c8c2d6f78f0edb0a85e522051387548e74531
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-09-08 00:06:28 +0530

    feat: Enhanced src/components/hr/Employee/EmployeeListTest.js,  - Detailed changes: diff --git a/src/components/hr/Employee/EmployeeLi... at  0:06:28.79 Mon 09/08/2025

---
+++ src/components/hr/Employee/EmployeeListTest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography, Container } from '@mui/material';
+
+const EmployeeListTest = () => {
+  return (
+    <Container>
+      <Typography variant="h4">
+        Employee Management Test - Working!
+      </Typography>
+    </Container>
+  );
+};
+
+export default EmployeeListTest;
