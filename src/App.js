ce6fa96832069caf98a62069da1142d6d943c969
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-09-07 20:14:24 +0530

    feat: Enhanced src/App.js,  - Detailed changes: diff --git a/src/App.js b/src/App.js index b6daa1a... at 20:14:24.75 Sun 09/07/2025

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Box } from '@mui/material'
@@ -43,24 +42,3 @@ function App() {
 }
 
 export default App
-=======
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Box } from '@mui/material';
-import PharmacyPOSFirebaseIntegrated from './pos/components/PharmacyPOSFirebaseIntegrated';
-import './App.css';
-
-function App() {
-  return (
-    <Box sx={{ height: '100vh', overflow: 'hidden' }}>
-      <Routes>
-        <Route path="/" element={<PharmacyPOSFirebaseIntegrated />} />
-        <Route path="/pos" element={<PharmacyPOSFirebaseIntegrated />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Box>
-  );
-}
-
-export default App;
->>>>>>> POS
